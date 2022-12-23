1b0b70278acb5be0fce35b04d6676d2035b2adfc
ArkadyStepin <stepin711@gmail.com>
2022-12-23 12:30:43 +0300

    work animat with addFile btn

--- src/pages/MusicPage.vue
+++ src/pages/MusicPage.vue
@@ -34,22 +34,80 @@ export default {
     addFile() {
       const musicFiles = this.$refs.musicFile.files;
       const audioControll = this.$refs.audioControll;
+      const canvas = this.$refs.canvasDiv;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      const ctx = canvas.getContext("2d");
+      let audioSource;
+      let analyser;
       audioControll.src = URL.createObjectURL(musicFiles[0]);
       audioControll.load();
-      audioControll.play();
+      // audioControll.play();
+      const audioContext = new AudioContext();
 
+      audioSource = audioContext.createMediaElementSource(audioControll);
+      console.log(audioControll);
+      analyser = audioContext.createAnalyser();
+      audioSource.connect(analyser);
+      analyser.connect(audioContext.destination);
+      analyser.fftSize = 2048;
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      const barWidth = canvas.width / 2.5 / bufferLength;
+      let barHeight;
+      let x;
+
+      function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
+        for (let i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i] * 2.5;
+          ctx.save(); // точка сохранения
+          ctx.translate(canvas.width / 2, canvas.height / 2);
+          ctx.rotate((i * Math.PI * 2.2) / bufferLength);
+
+          // // =======Один вид покраса===================
+
+          const red = i + barHeight / 35;
+          const green = barHeight / (i - 25);
+          const blue = barHeight + 50;
+          ctx.fillRect(0, 0, barWidth, 20);
+          ctx.fillStyle = "rgb(" + red + ", " + green + ", " + blue + ")";
+
+          // ===================Другой вид покраса(по ргб кругу)============
+
+          // const hue = i * 3;
+          // ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+
+          //=====================================
+
+          ctx.fillRect(0, 0, barWidth, barHeight);
+          x += barWidth;
+          ctx.restore();
+        }
+      }
+
+      function animate() {
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
+        requestAnimationFrame(animate);
+      }
+      animate();
       // console.log(audioControll);
     },
+
     // работает ток с 1 файлом, будь человеком, мапни все
     drop(e) {
       let reg = /audio/;
       let dropzoneFile = ref("");
       const audioControll = this.$refs.audioControll;
+      // const canvas = this.$refs.canvasDiv;
+
       dropzoneFile = e.dataTransfer.files[0];
       if (reg.test(dropzoneFile.type) === true) {
         audioControll.src = URL.createObjectURL(dropzoneFile);
         audioControll.load();
-        audioControll.play();
         console.log("zaebis");
       } else console.log("sosi");
     },
