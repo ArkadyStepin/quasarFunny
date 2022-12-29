be0b427456fb011dab950bd89ea3e9ad84db0f14
ArkadyStepin <stepin711@gmail.com>
2022-12-29 12:21:51 +0300

    ss

--- src/components/DropZone.vue
+++ src/components/DropZone.vue
@@ -26,13 +26,14 @@ import { ref } from "vue";
 export default {
   name: "DropZone",
 
-  setup() {
-    const active = ref(false);
-    const toggleActive = () => {
-      active.value = !active.value;
-    };
-    return { active, toggleActive };
-  },
+
+  // setup() {
+  //   const active = ref(false);
+  //   const toggleActive = () => {
+  //     active.value = !active.value;
+  //   };
+  //   return { active, toggleActive };
+  // },
 
   methods: {
     readFile() {
